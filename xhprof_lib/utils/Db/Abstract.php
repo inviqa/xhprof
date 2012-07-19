90d726bbaef55ac8719bc28c95fa192972cd111c
Alistair Stead <alistair.stead@designdisclosure.com>
2012-07-19 19:58:46 +0100

    Add implementation that will throw runtime exception

--- xhprof_lib/utils/Db/Abstract.php
+++ xhprof_lib/utils/Db/Abstract.php
@@ -12,4 +12,25 @@ abstract class Db_Abstract
     abstract public function query($sql);
     abstract public function escape($str);
     abstract public function affectedRows();
+
+    public static function unixTimestamp($field)
+    {
+        throw new RuntimeException(
+            "Method should be implemented '" . get_called_class() . "::" . __FUNCTION__ . "' in calling class"
+        );
+    }
+
+    public static function dateSub($days)
+    {
+        throw new RuntimeException(
+            "Method should be implemented '" . get_called_class() . "::" . __FUNCTION__ . "' in calling class"
+        );
+    }
+
+    public static function getNextAssoc($resultSet)
+    {
+        throw new RuntimeException(
+            "Method should be implemented '" . get_called_class() . "::" . __FUNCTION__ . "' in calling class"
+        );
+    }
 }
